228590613c2c96716eabfac38c1fc331ab86ecc0
kyungmin <02dlrudals@gmail.com>
2025-05-07 14:57:14 +0900

    Feat: Create Input Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,28 @@
+import Input from "./components/common/Input";
+import { useState } from "react";
+
 export default function App() {
-  return <></>;
+  const [value, setValue] = useState<string>("");
+
+  const onChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <>
+      <Input
+        type="text"
+        placeholder="아이디"
+        value={value}
+        onChange={onChangeValue}
+      />
+      <Input
+        type="text"
+        placeholder="아이디"
+        value={value}
+        onChange={onChangeValue}
+        edit
+      />
+    </>
+  );
 }
