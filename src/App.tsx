fbb07762c5d3dc7f4dba18106fd239e0c0f1defe
kyungmin <02dlrudals@gmail.com>
2025-05-10 19:40:21 +0900

    Feat: Create Emergency Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import EmergencyModal from "./modals/EmergencyModal";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import StreamingPage from "./pages/StreamingPage";
+import EmergencyPage from "./pages/EmergencyPage";
 
 export default function App() {
+  const nav = useNavigate();
+
+  const [isEmergency, setIsEmergency] = useState<boolean>(true);
+
+  const handleEmergency = () => {
+    setIsEmergency(false);
+    nav("/emergency");
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/streaming" element={<StreamingPage />} />
-    </Routes>
+    <>
+      {" "}
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/streaming" element={<StreamingPage />} />
+        <Route path="/emergency" element={<EmergencyPage />} />
+      </Routes>
+      {isEmergency && <EmergencyModal onClick={handleEmergency} />}
+    </>
   );
 }
