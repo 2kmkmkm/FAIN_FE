e61f8a830b6f3ff15c4602148c6fe2a3ea6867cb
kyungmin <02dlrudals@gmail.com>
2025-05-21 11:18:35 +0900

    Refactor: Create AppRoutes file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { useState } from "react";
+import AppRoutes from "./AppRoutes";
 import Navigation from "./components/common/Navigation";
 import EmergencyModal from "./modals/EmergencyModal";
-import MainPage from "./pages/MainPage";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import StreamingPage from "./pages/StreamingPage";
-import EmergencyPage from "./pages/EmergencyPage";
-import AnlaysisPage from "./pages/AnalysisPage";
-import HistoryPage from "./pages/HistoryPage";
-import HistoryDetailPage from "./pages/HistoryDetailPage";
-import MyPage from "./pages/MyPage";
-import PatientEditPage from "./pages/PatientEditPage";
-import GuardianEditPage from "./pages/GuardianEditPage";
 
 export default function App() {
   const nav = useNavigate();
+  const loc = useLocation();
 
   const [isEmergency, setIsEmergency] = useState<boolean>(false);
   const [selectedMenu, setSelectedMenu] = useState<string>("스트리밍");
+
+  const showNavigationPaths = ["/streaming", "/analysis", "/history", "/my"];
 
   const handleEmergency = () => {
     setIsEmergency(false);
@@ -27,26 +20,13 @@ export default function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/streaming" element={<StreamingPage />} />
-        <Route path="/emergency" element={<EmergencyPage />} />
-        <Route path="/analysis" element={<AnlaysisPage />} />
-        <Route path="/history" element={<HistoryPage />} />
-        <Route
-          path="/history/detail/:reportId"
-          element={<HistoryDetailPage />}
+      <AppRoutes />
+      {showNavigationPaths.some((path) => loc.pathname.startsWith(path)) && (
+        <Navigation
+          selectedMenu={selectedMenu}
+          setSelectedMenu={setSelectedMenu}
         />
-        <Route path="/my" element={<MyPage />} />
-        <Route path="/edit/guardian" element={<GuardianEditPage />} />
-        <Route path="/edit/patient" element={<PatientEditPage />} />
-      </Routes>
-      <Navigation
-        selectedMenu={selectedMenu}
-        setSelectedMenu={setSelectedMenu}
-      />
+      )}
       {isEmergency && <EmergencyModal onClick={handleEmergency} />}
     </>
   );
