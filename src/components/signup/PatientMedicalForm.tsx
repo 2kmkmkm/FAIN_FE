48a7e5f388416ac41138fd4cc3382343eae5c1f9
kyungmin <02dlrudals@gmail.com>
2025-05-21 11:42:50 +0900

    Style: Modify Popup position

--- src/components/signup/PatientMedicalForm.tsx
+++ src/components/signup/PatientMedicalForm.tsx
@@ -3,13 +3,14 @@ import search from "../../assets/search.svg";
 import information from "../../assets/information.svg";
 import Popup from "../common/Popup";
 import type { PatientMedicalFormProps } from "../../type/userType";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function PatientMedicalForm({
   medical,
   handleMedicalChange,
 }: PatientMedicalFormProps) {
   const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
+  const infoIconRef = useRef<HTMLImageElement>(null);
 
   return (
     <div className="flex flex-col gap-2.5">
@@ -17,12 +18,14 @@ export default function PatientMedicalForm({
         <div className="title flex w-fit">환자 의료 정보</div>
         <div className="relative w-fit">
           <img
+            ref={infoIconRef}
             src={information}
             className="w-4"
             onClick={() => setIsPopupOpen(true)}
           />
           {isPopupOpen && (
             <Popup
+              anchorRef={infoIconRef}
               onClose={() => setIsPopupOpen(false)}
               contents={`질환, 알러지, 복용약은 쉼표로 구분해서 적어주세요.\n주요 병원은 최대 1개까지 선택 가능합니다.`}
             />
