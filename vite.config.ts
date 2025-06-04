dc21b060fecc6a0a902ce7fabfcfc963ecae993e
kyungmin <02dlrudals@gmail.com>
2025-06-04 11:45:43 +0900

    Refactor: Update font size and weight

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
-
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
@@ -8,7 +7,12 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      includeAssets: [
+        'favicon.ico',
+        'fonts/Pretendard-Regular.woff2',
+        'fonts/Pretendard-Medium.woff2',
+        'fonts/Pretendard-SemiBold.woff2',
+      ],
       manifest: {
         name: 'FAIN',
         short_name: 'FAIN',
@@ -23,13 +27,22 @@ export default defineConfig({
             src: '/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
+            purpose: 'any maskable',
           },
           {
             src: '/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-          }
+            purpose: 'any maskable',
+          },
         ],
+      },
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
+      },
+      devOptions: {
+        enabled: true,
       },
     }),
   ],
